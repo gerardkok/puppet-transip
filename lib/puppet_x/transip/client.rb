162467a1b77b4c027e3d84bcc0fb28f0e4a68377
Gerard Kok <gerard@gerardkok.nl>
2017-12-14 20:15:04 +0100

    debug

--- lib/puppet_x/transip/client.rb
+++ lib/puppet_x/transip/client.rb
@@ -78,6 +78,7 @@ module Transip
     end
 
     def self.all_entries
+      puts "in all_entries"
       entries = domainclient.request(:batch_get_info, domain_names: domain_names).map(&:to_hash)
       puts "entries: #{entries}\n"
       entries.inject([]) do |memo, domain|
