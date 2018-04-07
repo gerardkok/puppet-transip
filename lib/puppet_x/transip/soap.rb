0ba4792d29694ea93dcc054196af0e4c27d82528
Gerard Kok <gerard@gerardkok.nl>
2018-04-07 22:57:46 +0200

    adding tests

--- lib/puppet_x/transip/soap.rb
+++ lib/puppet_x/transip/soap.rb
@@ -13,7 +13,22 @@ module Transip
     NAMESPACES ||= { :'xmlns:enc' => 'http://schemas.xmlsoap.org/soap/encoding/' }.freeze
 
     class << self
-      def from_hash(hash)
+      def from_soap(input)
+        case input
+        when Array
+          array_from_soap(input)
+        when Hash
+          hash_from_soap(input)
+        else
+          input
+        end
+      end
+
+      def array_from_soap(input)
+        input.map { |value| from_soap(value) }
+      end
+
+      def hash_from_soap(hash)
         if hash.keys.include?(:return)
           from_soap(hash[:return])
         elsif hash.keys.include?(:item)
@@ -29,68 +44,6 @@ module Transip
         end
       end
       
-      def from_soap(input)
-        case input
-        when Array
-          input.map { |value| from_soap(value) }
-        when Hash
-          from_hash(input)
-        else
-          input
-        end
-      end
-
-      def camelize(word)
-        parts = word.to_s.split('_')
-        parts.first.downcase + parts[1..-1].map{ |p| p.capitalize }.join
-      end
-  
-      def array_to_indexed_hash(array)
-        Hash[(0...array.size).zip(array)]
-      end
-      
-      def urlencode(input)
-        URI.encode_www_form_component(input.to_s).gsub('+', '%20').gsub('%7E', '~').gsub('*', '%2A')
-      end
-  
-      def encode(params, prefix = nil)
-        case params
-        when Hash
-          params.map do |key, value|
-            encoded_key = prefix.nil? ? urlencode(key) : "#{prefix}[#{urlencode(key)}]"
-            encode(value, encoded_key)
-          end
-        when Array
-          h = array_to_indexed_hash(params)
-          encode(h, prefix)
-        else
-          ["#{prefix}=#{urlencode(params)}"]
-        end
-      end
-  
-      def message_options(method, api_service, hostname, time, nonce)
-        %W[ __method=#{camelize(method)} __service=#{api_service} __hostname=#{hostname} __timestamp=#{time} __nonce=#{nonce} ]
-      end
-  
-      def sign(input, private_key)
-        digest = OpenSSL::Digest::SHA512.new
-        signed_input = private_key.sign(digest, input)
-        urlencode(Base64.encode64(signed_input))
-      end
-  
-      def to_cookie_array(username, mode, time, nonce, api_version, signature)
-        %W[ login=#{username} mode=#{mode} timestamp=#{time} nonce=#{nonce} clientVersion=#{api_version} signature=#{signature} ]
-      end
-  
-      def cookies(action, username, mode, api_service, api_version, hostname, private_key, options = {})
-        time = Time.new.to_i
-        #strip out the -'s because transip requires the nonce to be between 6 and 32 chars
-        nonce = SecureRandom.uuid.gsub("-", '')
-        serialized_input = (encode(options.values) + message_options(action, api_service, hostname, time, nonce)).join('&')
-        signature = sign(serialized_input, private_key)
-        to_cookie_array(username, mode, time, nonce, api_version, signature).map { |c| HTTPI::Cookie.new(c) }
-      end
-  
       def to_soap(options)
         case options
         when Array
@@ -120,8 +73,63 @@ module Transip
           memo[k] = to_soap(v)
         end
       end
-    end
 
+      def camelize(word)
+        parts = word.to_s.split('_')
+        parts.first.downcase + parts[1..-1].map{ |p| p.capitalize }.join
+      end
+  
+      def array_to_indexed_hash(array)
+        Hash[(0...array.size).zip(array)]
+      end
+      
+      def urlencode(input)
+        URI.encode_www_form_component(input.to_s).gsub('+', '%20').gsub('%7E', '~').gsub('*', '%2A')
+      end
+  
+      def encode(params, prefix = nil)
+        case params
+        when Hash
+          params.map do |key, value|
+            encoded_key = prefix.nil? ? urlencode(key) : "#{prefix}[#{urlencode(key)}]"
+            encode(value, encoded_key)
+          end.flatten
+        when Array
+          h = array_to_indexed_hash(params)
+          encode(h, prefix)
+        else
+          ["#{prefix}=#{urlencode(params)}"]
+        end
+      end
+  
+      def message_options(method, api_service, hostname, time, nonce)
+        %W[ __method=#{camelize(method)} __service=#{api_service} __hostname=#{hostname} __timestamp=#{time} __nonce=#{nonce} ]
+      end
+
+      def serialize(action, api_service, hostname, time, nonce, options = {})
+        (encode(options.values) + message_options(action, api_service, hostname, time, nonce)).join('&')
+      end
+  
+      def sign(input, private_key)
+        digest = OpenSSL::Digest::SHA512.new
+        signed_input = private_key.sign(digest, input)
+        urlencode(Base64.encode64(signed_input))
+      end
+  
+      def to_cookie_array(username, mode, time, nonce, api_version, signature)
+        %W[ login=#{username} mode=#{mode} timestamp=#{time} nonce=#{nonce} clientVersion=#{api_version} signature=#{signature} ]
+      end
+  
+      def cookies(action, username, mode, api_service, api_version, hostname, private_key, options = {})
+        time = Time.new.to_i
+        # strip out the -'s because transip requires the nonce to be between 6 and 32 chars
+        nonce = SecureRandom.uuid.gsub("-", '')
+        serialized_input = serialize(action, api_service, hostname, time, nonce, options)
+        signature = sign(serialized_input, private_key)
+        to_cookie_array(username, mode, time, nonce, api_version, signature).map { |c| HTTPI::Cookie.new(c) }
+      end
+    end
+  
     def initialize(options = {})
       key = options[:key] || (options[:key_file] && File.read(options[:key_file]))
       raise ArgumentError, 'Invalid RSA key' unless key =~ /-----BEGIN (RSA )?PRIVATE KEY-----(.*)-----END (RSA )?PRIVATE KEY-----/sim
--- spec/unit/puppet_x/transip/soap_spec.rb
+++ spec/unit/puppet_x/transip/soap_spec.rb
@@ -8,16 +8,80 @@ describe Transip::Soap do
   end
 
   context 'array_to_indexed_hash' do
-    it 'should be able to index empty array' do
+    it 'should index empty array' do
       expect(Transip::Soap.array_to_indexed_hash([])).to eq({})
     end
 
-    it 'should be able to index single element array' do
+    it 'should index single element array' do
       expect(Transip::Soap.array_to_indexed_hash(['a'])).to eq({0 => 'a'})
     end
 
-    it 'should be able to index multi element array' do
+    it 'should index multi element array' do
       expect(Transip::Soap.array_to_indexed_hash(['a', 'b', 'c'])).to eq({0 => 'a', 1 => 'b', 2 => 'c'})
+    end
+  end
+
+  context 'encode' do
+    it 'should encode array' do
+      expect(Transip::Soap.encode(['a', 'b'])).to eq(['0=a', '1=b'])
+    end
+ 
+    it 'should encode array of array' do
+      expect(Transip::Soap.encode([['a', 'b']])).to eq(['0[0]=a', '0[1]=b'])
+    end
+
+    it 'should encode hash' do
+      expect(Transip::Soap.encode({name: 'a', type: 'b'})).to eq(['name=a', 'type=b'])
+    end
+
+    it 'should encode array of hash' do
+      expect(Transip::Soap.encode([{name: 'a', type: 'b'}])).to eq(['0[name]=a', '0[type]=b'])
+    end
+
+    it 'should encode array of array of hash' do
+      expect(Transip::Soap.encode([[{name: 'a', type: 'b'}]])).to eq(['0[0][name]=a', '0[0][type]=b'])
+    end
+  end
+
+  context 'serialize' do
+    let(:output) { '0=a&1[0][name]=a&1[0][type]=b&__method=action&__service=service&__hostname=endpoint&__timestamp=1&__nonce=42' }
+    it 'should serialize' do
+      expect(Transip::Soap.serialize(:action, 'service', 'endpoint', 1, 42, options = { param1: 'a', param2: [{ name: 'a', type: 'b' }]})).to eq(output)
+    end
+  end
+
+  context 'to_soap' do
+    let(:output) do
+      { :item => { :content! => ['a', 'b'], :'@xsi:type' => 'tns:String' },
+        :'@xsi:type' => 'tns:ArrayOfString',
+        :'@enc:arrayType' => 'tns:String[2]' }
+    end
+    it 'should convert array to soap' do
+      expect(Transip::Soap.to_soap(['a', 'b'])).to eq(output)
+    end
+  end
+
+  context 'single element array from_soap' do
+    let(:input) do
+      { :item => { :value => 'a', :'@xsi:type' => 'tns:String' },
+        :'@xsi:type' => 'tns:ArrayOfString',
+        :'@soap_enc:array_type' => 'tns:String[1]' }
+    end
+    it 'should convert single element array' do
+      expect(Transip::Soap.from_soap(input)).to eq([{ value: 'a' }])
+    end
+  end
+
+  context 'mutli element array from_soap' do
+    let(:input) do
+      { :item => [
+          { :value => 'a', :'@xsi:type' => 'tns:String' },
+          { :value => 'b', :'@xsi:type' => 'tns:String' }],
+        :'@xsi:type' => 'tns:ArrayOfString',
+        :'@soap_enc:array_type' => 'tns:String[2]' }
+    end
+    it 'should convert single element array' do
+      expect(Transip::Soap.from_soap(input)).to eq([{ value: 'a' }, { value: 'b' }])
     end
   end
 end
